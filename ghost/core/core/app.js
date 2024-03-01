137986abbc0eecc257c929b68e69e1066d9129f8
Fabien O'Carroll <fabien@allou.is>
2024-03-01 10:55:20 -0500

    Initial implementation

--- ghost/core/core/app.js
+++ ghost/core/core/app.js
@@ -5,6 +5,7 @@ const urlService = require('./server/services/url');
 
 const fs = require('fs');
 const path = require('path');
+const EventEmitter = require('events');
 
 const isMaintenanceModeEnabled = (req) => {
     if (req.app.get('maintenance') || config.get('maintenance').enabled || !urlService.hasFinished()) {
@@ -27,6 +28,89 @@ const maintenanceMiddleware = (req, res, next) => {
     fs.createReadStream(path.resolve(__dirname, './server/views/maintenance.html')).pipe(res);
 };
 
+function createRequestBatchingMiddleware(options = {}) {
+    /**
+     * @type {Map.<string, {req: import('express').Request, res: import('express').Response, next: import('express').NextFunction}[]>}
+     */
+    const globalRequestMap = new Map();
+
+    const shouldBatchRequest = options.shouldBatchRequest || function shouldBatchRequest(req) {
+        return true;
+    };
+
+    const getKeyForRequest = options.getKeyForRequest || function getKeyForRequest(req) {
+        return req.method + req.url;
+    };
+
+    /**
+     *
+     * @param {string} key
+     * @param {Map.<string, {req: import('express').Request, res: import('express').Response, next: import('express').NextFunction}[]>} requestMap
+     * @returns
+     */
+    function runNextRequest(key, requestMap) {
+        const requests = requestMap.get(key);
+        const requestToRun = requests.shift();
+        function onError() {
+            runNextRequest(key, requestMap);
+        }
+        function onSuccess(statusCode, headers, data) {
+            requestMap.delete(key);
+            for (const request of requests) {
+                process.nextTick(() => {
+                    request.res.statusCode = statusCode;
+                    request.res.set(headers);
+                    request.res.send(data);
+                });
+            }
+        }
+        patchResponse(requestToRun, onError, onSuccess);
+        requestToRun.next();
+    }
+
+    /**
+     * @param {object} data
+     * @param {import('express').Response} data.res
+     */
+    function patchResponse({res}, onError, onSuccess) {
+        const expressSend = res.send;
+        let handled = false;
+        res.on('close', function () {
+            if (handled) {
+                return;
+            }
+            onError();
+        });
+
+        res.send = function patchedSend(data) {
+            handled = true;
+            const statusCode = res.statusCode;
+            const headers = res.getHeaders();
+            onSuccess(statusCode, headers, data);
+            return expressSend.call(res, data);
+        };
+    }
+
+    return function requestBatchingHandler(req, res, next) {
+        if (!shouldBatchRequest(req)) {
+            return next();
+        }
+        const key = getKeyForRequest(req);
+        if (!key) {
+            return next();
+        }
+
+        let similarRequests = globalRequestMap.get(key);
+        if (!similarRequests) {
+            similarRequests = [{req, res, next}];
+            globalRequestMap.set(key, similarRequests);
+            return runNextRequest(key, globalRequestMap);
+        } else {
+            similarRequests.push({req, res, next});
+        }
+    };
+}
+
 const rootApp = () => {
     const app = express('root');
     app.use(sentry.requestHandler);
@@ -35,6 +119,14 @@ const rootApp = () => {
     }
     app.enable('maintenance');
     app.use(maintenanceMiddleware);
+    app.use(createRequestBatchingMiddleware({
+        shouldBatchRequest(req) {
+            return req.method === 'GET' && !req.url.startsWith('/ghost') && !req.url.startsWith('/content');
+        },
+        getKeyForRequest(req) {
+            return req.method + req.url;
+        }
+    }));
 
     return app;
 };
