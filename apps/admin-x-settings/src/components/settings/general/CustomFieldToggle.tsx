b4179de80ffa9a679ea1d38138e9c783405f8595
Aileen Booker <AileenCGN@gmail.com>
2024-03-03 17:06:13 -0500

    Basic first styles

--- apps/admin-x-settings/src/components/settings/general/CustomFieldToggle.tsx
+++ apps/admin-x-settings/src/components/settings/general/CustomFieldToggle.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import {Icon} from '@tryghost/admin-x-design-system';
 import {withErrorBoundary} from '@tryghost/admin-x-design-system';
 
 const CustomFieldToggle: React.FC<{
     icon?: URL,
     name: string,
-    type?: string,
+    type?: 'url' | 'short' | 'long' | 'boolean',
     placeholder?: string,
     enabled: boolean
 }> = ({
@@ -13,17 +14,36 @@ const CustomFieldToggle: React.FC<{
     type,
     enabled
 }) => {
+    let iconComponent;
+
+    if (!icon) {
+        switch (type) {
+        case 'long':
+            iconComponent = <Icon className='absolute left-3 top-3 z-10' colorClass='text-grey-500' name='hyperlink-circle' size='sm' />;
+            break;
+        case 'short':
+            iconComponent = <Icon className='absolute left-3 top-3 z-10' colorClass='text-grey-500' name='hyperlink-circle' size='sm' />;
+            break;
+        case 'url':
+            iconComponent = <Icon className='absolute left-3 top-3 z-10' colorClass='text-grey-500' name='hyperlink-circle' size='sm' />;
+            break;
+        case 'boolean':
+            iconComponent = <Icon className='absolute left-3 top-3 z-10' colorClass='text-grey-500' name='hyperlink-circle' size='sm' />;
+            break;
+        default:
+            break;
+        }
+    } else {
+        iconComponent = <img className='absolute left-3 top-3 z-10' src={icon.href} />;
+    }
+
     return (
-        <div>
-            <div>
-                <img src={icon} />
-                <div>
-                    <strong>
-                        {name}
-                    </strong>
-                    <p>
-                        {type}
-                    </p>
+        <div className='flex justify-between border-y-grey-250'>
+            <div className='flex'>
+                {iconComponent}
+                <div className='wrap flex'>
+                    <strong>{name}</strong>
+                    <p>{type}</p>
                 </div>
             </div>
             <div>
